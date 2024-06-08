c378e7b5d7e907da9da1153df55452354ecc66f3
Behzad Barzin <behzad_barzin@live.com>
2024-06-09 01:00:07 +0330

    Meeting page basic setup

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,10 @@
-import React, { FC } from "react";
+"use client";
+
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { FC, useState } from "react";
 
 interface IProps {
   params: {
@@ -7,7 +13,22 @@ interface IProps {
 }
 
 const Meeting: FC<IProps> = ({ params: { id: meetingId } }) => {
-  return <div>Meeting: {meetingId}</div>;
+  // ---------------------------------------------------------------------------
+  // Currently authenticated user
+  const { user, isLoaded } = useUser();
+  // ---------------------------------------------------------------------------
+  // Is video and audio setup completed?
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+  // ---------------------------------------------------------------------------
+  return (
+    <main className="h-screen w-full">
+      <StreamCall>
+        <StreamTheme>
+          {!isSetupCompleted ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
