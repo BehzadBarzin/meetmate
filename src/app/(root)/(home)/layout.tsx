c18c0452ff92abcaa6f432ac98c4afa862ed49bd
Behzad Barzin <behzad_barzin@live.com>
2024-06-08 17:06:44 +0330

    Adds necessary assets

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 export default function HomeLayout({
   children,
 }: Readonly<{
@@ -5,12 +8,17 @@ export default function HomeLayout({
 }>) {
   return (
     <main className="relative">
-      Navbar
+      {/* Navbar------------------------------------------------------------ */}
+      <Navbar />
+      {/* Sidebar + Content------------------------------------------------- */}
       <div className="flex">
-        Sidebar
+        {/* Sidebar--------------------------------------------------------- */}
+        <Sidebar />
+        {/* Content--------------------------------------------------------- */}
         <section className="flex flex-col min-h-screen flex-1 px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
+        {/* ---------------------------------------------------------------- */}
       </div>
     </main>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <section className="sticky left-0 flex flex-col justify-between bg-dark-1 h-screen w-fit p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-col flex-1 gap-6"></div>
+    </section>
+  );
+};
+
+export default Sidebar;
