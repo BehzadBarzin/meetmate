241aeac575fd86eec911f89afcc59bfd2d9488aa
Behzad Barzin <behzad_barzin@live.com>
2024-06-08 15:39:27 +0330

    Add ShadCN to website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-extrabold">Hello World!</h1>
+      <h1 className="text-6xl font-extrabold m-6 ">Hello World!</h1>
+      <Button>Click Me!</Button>
     </main>
   );
 }
